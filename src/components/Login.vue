89d28771839e91fd5a2b042dc4185cd38886d6d0
wxal <296059956@qq.com>
2019-02-15 11:58:44 +0800

    用户登录功能实现完毕

---
+++ src/components/Login.vue
@@ -0,0 +1,102 @@
+<template>
+  <div id="login-container">
+    <div id="login-box">
+      <div id="logo-box">
+        <img src="../assets/img/logo.png" alt>
+      </div>
+      <el-form :rules="loginFormRules" :model="loginForm" ref="loginFormRef">
+        <el-form-item prop="username">
+          <el-input v-model="loginForm.username">
+            <i slot="prefix" class="iconfont icon-user"></i>
+          </el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input v-model="loginForm.password" type="password">
+            <i slot="prefix" class="iconfont icon-3702mima"></i>
+          </el-input>
+        </el-form-item>
+        <el-row>
+          <el-col :offset="15">
+            <el-button type="primary" @click="login">登录</el-button>
+            <el-button type="info" @click="resetForm">重置</el-button>
+          </el-col>
+        </el-row>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      loginForm: { username: '', password: '' },
+      loginFormRules: {
+        username: [
+          { required: true, message: '请输入用户名称', trigger: 'blur' }
+        ],
+        password: [{ required: true, message: '请输入密码', trigger: 'blur' }]
+      }
+    }
+  },
+  methods: {
+    login() {
+      this.$refs.loginFormRef.validate(async valid => {
+        if (valid === true) {
+          const { data: res } = await this.$http.post('/login', this.loginForm)
+          if (res.meta.status !== 200) {
+            return this.$message.error('用户名或密码不存在')
+          }
+          window.sessionStorage.setItem('token', res.pushdata.token)
+          this.$router.push('/home')
+        }
+      })
+    },
+    resetForm() {
+      this.$refs.loginFormRef.resetFields()
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+#login-container {
+  background-color: #2b4b6b;
+  height: 100%;
+  overflow: hidden;
+  #login-box {
+    width: 450px;
+    height: 304px;
+    background-color: #fff;
+    border-radius: 4px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    .el-form {
+      position: absolute;
+      bottom: 0;
+      width: 100%;
+      padding: 20px;
+      box-sizing: border-box;
+    }
+    #logo-box {
+      width: 130px;
+      height: 130px;
+      border: 1px solid #eee;
+      border-radius: 50%;
+      padding: 8px;
+      box-shadow: 0 0 10px #eee;
+      position: absolute;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      background-color: #fff;
+      img {
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+        background-color: #eee;
+      }
+    }
+  }
+}
+</style>
